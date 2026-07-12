d372debed0b20956164ff346a22fb787bd7b9723
TeTy-GH <tetchell@gmail.com>
2026-07-12 22:16:43 +0900

    設定ドロワー化7

--- src/components/SettingDrawer.tsx
+++ src/components/SettingDrawer.tsx
@@ -39,9 +39,9 @@ export const SettingDrawer = ({
       />
         
       {/* ドロワー本体 */}
-      <div className={`fixed top-0 right-0 h-full w-[25%] bg-white dark:bg-zinc-900 z-50 shadow-2xl 
-                      p-6 transition-transform duration-300 ease-in-out
-                      ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+      <div className={`fixed top-0 right-0 h-full w-[40%] md:w-80 bg-white dark:bg-zinc-900 z-50 
+                      shadow-2xl p-6 transition-transform duration-300 ease-in-out md:top-0 md:right-0 md:h-full md:w-96 md:rounded-none
+                      ${isOpen ? 'translate-y-0 md:translate-x-0' : 'translate-y-full md:translate-x-full'}`}>
         <div className='flex justify-end'>
           <button onClick={onClose} className={`mb-4 text-slate-500 dark:text-white ${currentStyles.masterText}`}>
             ✕ 閉じる
